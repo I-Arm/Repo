5ef4154c536bd3b5d5d74448df5a8712a689c07d
Chirapat123 <chiraphat.v@ku.th>
2025-07-03 17:16:51 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -5,15 +5,41 @@ public class PasswordValidator {
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param password string ที่ต้องการตรวจสอบ
+     * @return PasswordStrength INVALID อื่นๆ
      */
     // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
-        
+    public static PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength 
+
+
         // ส่วนของ Implementation ที่คุณต้องเขียน
         // ...
+        char[] charArray = password.toCharArray();
+        int upchar=0 , lowchar=0 , number=0 , other=0 , num=0;
+        if(password.length()<8)
+            return PasswordStrength.INVALID;
+        else {
+            for(int i=0; i<password.length(); i++) {
+                if(charArray[i]>='A'&&charArray[i]<='Z') 
+                    upchar++;
+                else if(charArray[i]>='a'&&charArray[i]<='z') 
+                    lowchar++;
+                else if(charArray[i]>='0'&&charArray[i]<='9') 
+                    number++;
+                else other++;
+            }
+            if(upchar!=0) num++;
+            if(lowchar!=0) num++;
+            if(number!=0) num++;
+            if(other!=0) num++;
+            
+            if(num==1||num==2) return PasswordStrength.WEAK;
+            if(num==3) return PasswordStrength.MEDIUM;
+            if(num==4) return PasswordStrength.STRONG;
+        }
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
+        int minLength = 8; // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
         
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        return PasswordStrength.WEAK ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -6,7 +6,7 @@ public class PasswordValidatorTest{
         System.out.println("--- Running Password Validator Tests ---");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        PasswordStrength result1 = PasswordValidator.validate("1234567");
         if (result1 == PasswordStrength.INVALID) {
             System.out.println("Test Case 1 Passed: Short password is INVALID.");
         } else {
@@ -14,6 +14,26 @@ public class PasswordValidatorTest{
         }
 
         // --- เขียน Test Case อื่นๆ ต่อ ---
+        PasswordStrength result2 = PasswordValidator.validate("12N");
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: Short password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected WEAK but got " + result2);
+        }
+        PasswordStrength result3 = PasswordValidator.validate("DGHDH8544#FGFa");
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed: Short password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected MEDIUM but got " + result3);
+        }
+        PasswordStrength result4 = PasswordValidator.validate("AAAAAAAAA44444444444");
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 Passed: Short password is STRONG.");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected STRONG but got " + result4);
+        }
+
+
 
         System.out.println("--------------------------------");
     }
