06766fc0d78e70496a8ced1de9149dee3f8996ae
1alexqt <jakkarin.pe@ku.th>
2025-07-03 17:54:29 +0700

    Add files via upload

--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -12,8 +12,35 @@ public class PasswordValidatorTest{
         } else {
             System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
         }
+        // Test Case 2: รหัสผ่านที่มีแค่ตัวอักษรตัวเล็กควรจะ WEAK
+        PasswordStrength result2 = PasswordValidator.validate("ishowspeed");
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: Only letter password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected INVALID but got " + result2);
+        }
+        // Test Case 3: รหัสผ่านที่มีแค่ตัวเลขควรจะ WEAK
+        PasswordStrength result3 = PasswordValidator.validate("0647831726");
+        if (result3 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 3 Passed: Only digit password is WEAK.");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected INVALID but got " + result3);
+        }
+        // Test Case 4: รหัสผ่านที่มีตัวเลขและตัวอักษรพิมพ์เล็กควรจะ MEDIUM
+        PasswordStrength result4 = PasswordValidator.validate("031047alexqt");
+        if (result4 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 4 Passed: Small letter and digit is MEDIUM.");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected INVALID but got " + result4);
+        }
+        // Test Case 5: รหัสผ่านที่มีทั้งตัวเลข,ตัวอักษรพิมพ์ใหญ่และเล็ก,ตัวอักษรพิเศษ ควรจะ STRONG
+        PasswordStrength result5 = PasswordValidator.validate("Lekkung2004!");
+        if (result5 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 5 Passed: Big letter , small letter , digit and special letter is STRONG.");
+        } else {
+            System.out.println("Test Case 5 FAILED: Expected INVALID but got " + result5);
+        }
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
 
         System.out.println("--------------------------------");
     }
